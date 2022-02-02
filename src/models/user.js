93c92b0cbc3ede58c0e54c1bbd969b9be0dbc3a2
andres085 <andresito.martinez1985@gmail.com>
2022-02-02 20:33:19 -0300

    Se borro el password-utils y se agregaron esos metodos al Modelo de Usuario recien creado

---
+++ src/models/user.js
@@ -0,0 +1,67 @@
+import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+export default (sequelize) => {
+    class User extends Model {
+
+        static associate(models) {
+            
+        }
+
+        static async hashPassword(password) {
+            return bcrypt.hash(password, environment.saltRounds);
+        }
+
+        static async comparePasswords(password, hashedPassword) {
+            return bcrypt.compare(password, hashedPassword);
+        }
+    }
+
+    User.init({
+        email: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: 'Not a valid email address'
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING(50),
+            unique: true
+        },
+        firstName: {
+            type: DataTypes.STRING(50),
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'First name has too many characters'
+                }
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING(50),
+            validate: {
+                len: {
+                    args: [0, 50],
+                    msg: 'Last name has too many characters'
+                }
+            }
+        }
+    }, {
+        sequelize,
+        modelName: 'User',
+        indexes: [{unique: true, fields: ['email']}],
+    });
+
+    User.beforeSave(async (user, options) => {
+        const hashedPassword = await User.hashPassword(user.password);
+        user.password = hashedPassword;
+    });
+};
