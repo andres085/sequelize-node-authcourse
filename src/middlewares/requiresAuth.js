15b69568500f171a4b41bc6d6537889cbae4e9bc
andres085 <andresito.martinez1985@gmail.com>
2022-02-16 17:54:37 -0300

    Se armo un middleware para controlar el token de autorizacion

---
+++ src/middlewares/requiresAuth.js
@@ -0,0 +1,51 @@
+import JWTUtils from '../utils/jwt-utils';
+
+function requiresAuth(tokenType = 'accessToken') {
+    return function (req, res, next) {
+        const authHeader = req.headers.authorization
+
+        if (authHeader) {
+            try {
+                
+                var [bearer, token] = authHeader.split(' ');
+
+                if (bearer.toLowerCase() !== 'bearer' || !token) {
+                    throw Error;
+                }
+
+            } catch (error) {
+                return res.status(401).send({
+                success: false,
+                message: 'Wrong bearer token'
+            });
+            }
+         
+        } else {
+            return res.status(401).send({
+                success: false,
+                message: 'Authorization header not found'
+            });
+        }
+
+        try {
+            let jwt;
+            switch (tokenType) {
+                case 'refreshToken':
+                    jwt = JWTUtils.verifyRefreshtoken(token);
+                case 'accessToken':
+                default:
+                    jwt = JWTUtils.verifyAccessToken(token);
+            }
+            req.body.jwt = jwt;
+            next();
+        } catch (error) {
+              return res.status(401).send({
+                success: false,
+                message: 'Invalid Token'
+            });
+        }
+        
+    }
+}
+
+export default requiresAuth;
