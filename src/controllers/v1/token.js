3acd9ded1c3ef8ee1218a4eec97fbfb6776c237b
andres085 <andresito.martinez1985@gmail.com>
2022-02-25 19:56:00 -0300

    Se agrego el controlador para el token

---
+++ src/controllers/v1/token.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import models from '../../models';
+import JWTUtils from '../../utils/jwt-utils';
+import runAsyncWrapper from '../../utils/runAsyncWrapper';
+
+const router = Router();
+
+const { User } = models;
+
+router.post('/token', requiresAuth('refreshToken'), runAsyncWrapper(async (req, res) => {
+    const { jwt } = req.body;
+    const user = await User.findOne({ where: { email: jwt.email }, include: RefreshToken });
+    const savedToken = user.RefreshToken;
+    if (!savedToken || !savedToken.token) {
+        return res.status(401).send({
+            success: false,
+            message: 'You must log in first'
+        });
+    }
+    const payload = { email: user.email };
+    const newAccessToken = JWTUtils.generateAcessToken(payload);
+    return res.status(200).send({ success: true, data: { accessToken: newAccessToken } });
+
+}));
+
+export default router;
