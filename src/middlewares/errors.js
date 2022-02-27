6f42d7c5ff0e2e1d442530aee3fbadbd285b6069
andres085 <andresito.martinez1985@gmail.com>
2022-02-26 21:12:00 -0300

    Se modifico el middleware para errores y se realizo un test en el archivo token para verificar que el middleware de errores funciona

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -1,6 +1,4 @@
-function errorsMiddleware(err, req, res, next) {
-    console.error('[Errors Middleware]: \n', err.stack);
-    res.status(500).send({ succcess: false, message: error.message });
+export default function errorsMiddleware(err, req, res, next) {
+  console.error('Error in errors middleware:\n', err.stack);
+  res.status(500).send({ success: false, message: err.message });
 }
-
-export default errorsMiddleware;
--- tests/controllers/v1/token.test.js
+++ tests/controllers/v1/token.test.js
@@ -41,6 +41,20 @@ describe('token', () => {
         });
     });
 
+    describe('errors middleware', () => {
+        it('should return 500 if something went wrong', async () => {
+            const jwtUtilsSpy = jest.spyOn(JWTUtils, 'generateAcessToken');
+            jwtUtilsSpy.mockImplementation(() => {
+                throw Error('test error');
+            });
+            const refreshToken = newUserResponse.body.data.refreshToken;
+            const response = await request(app).post('/v1/token').set('Authorization', `Bearer ${refreshToken}`).send().expect(500);
+            jwtUtilsSpy.mockRestore();
+            expect(response.body.success).toEqual(false);
+            expect(response.body.message).toEqual('test error');
+        });
+    });
+
     it('should get a new access token successfully', async () => {
         const refreshToken = newUserResponse.body.data.refreshToken;
         const response = await request(app).post('/v1/token').set('Authorization', `Bearer ${refreshToken}`).send().expect(200);
